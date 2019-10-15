0bc4b6d55f8452d933af67266e96cc5be0fce80b
Hojjat Seyed <hojjatseyed@gmail.com>
2019-10-15 12:17:26 +0200

    setHealth method changed & tests added for this

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,39 @@
 public class Player {
+
+    private int speed;
+    private int health;
+    final int MAX_HEALTH = 100;
+
+
+    public Player(){
+        speed = 10;
+        health = MAX_HEALTH;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        if (health < 0 || health > MAX_HEALTH)
+            throw new IllegalArgumentException("Health can't be less than zero (0)");
+        this.health = health;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "speed=" + speed +
+                ", health=" + health +
+                '}';
+    }
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,2 +1,65 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PlayerTest {
+    Player p = new Player();
+
+    @Test
+    void constructorCall() {
+        assertEquals(p.getHealth(), 100);
+        assertEquals(p.getSpeed(), 10);
+    }
+
+    @Test
+    void getHealth(){
+        assertEquals(p.getHealth(),100);
+    }
+
+    @Test
+    void getSpeed(){
+        assertEquals(p.getSpeed(),10);
+
+    }
+
+    @Test
+    void setHealthToPositiveValue(){
+        int newVal = 90;
+        p.setHealth(newVal);
+       assertEquals(p.getHealth(),newVal);
+    }
+
+    @Test
+    void setHealthToNegativeValue(){
+        int newVal = -10;
+        assertThrows(IllegalArgumentException.class, () -> {
+        p.setHealth(newVal);
+        });
+    }
+
+    @Test
+    void setHealthToOverMaxHealth(){
+        int newVal = 101;
+        assertThrows(IllegalArgumentException.class, () -> {
+            p.setHealth(newVal);
+        });
+    }
+
+    @Test
+    void setSpeedToPositiveValue(){
+        int newVal = 5;
+        p.setSpeed(newVal);
+        assertEquals(p.getSpeed(),newVal);
+    }
+
+
+
+
+
+
+
+
+
+
+
 }

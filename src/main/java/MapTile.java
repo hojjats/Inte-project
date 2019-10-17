51cc65f85ceaa9c54996e1a5c030e47f5ed73aa2
Hojjat Seyed <hojjatseyed@gmail.com>
2019-10-17 11:18:26 +0200

    Added name to MapTile and added Tests accordingly

--- src/main/java/MapTile.java
+++ src/main/java/MapTile.java
@@ -2,17 +2,23 @@ public class MapTile {
 
     private Effect effect;
     private Position position;
+    private String name;
     private MapTile northTile;
     private MapTile southTile;
     private MapTile eastTile;
     private MapTile westTile;
 
     // Effect can be null, no effect on tile
-    public MapTile(Effect effect, Position position) {
+    public MapTile(Effect effect, Position position, String name) {
         if(position == null)
             throw new IllegalArgumentException("Position can't be null ");
+        if (name == null)
+            throw new IllegalArgumentException("Name can't be null");
+        if (name.equals(""))
+            throw new IllegalArgumentException("Name can't be empty");
         this.effect = effect;
         this.position = position;
+        this.name = name;
     }
 
     public Effect getEffect() {
@@ -21,5 +27,9 @@ public class MapTile {
 
     public Position getPosition() {
         return position;
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/test/java/MapTileTest.java
+++ src/test/java/MapTileTest.java
@@ -8,7 +8,7 @@ public class MapTileTest {
     void createMapTile() {
         Effect e = new Effect();
         Position pos = new Position(0, 0);
-        MapTile mapTile = new MapTile(e, pos);
+        MapTile mapTile = new MapTile(e, pos,"HeroStorm");
         assertEquals(mapTile.getEffect(), e);
         assertEquals(mapTile.getPosition(), pos);
     }
@@ -16,30 +16,47 @@ public class MapTileTest {
     @Test
     void createMapTileNullEffect() {
         Position pos = new Position(0, 0);
-        MapTile mapTile = new MapTile(null, pos);
+        MapTile mapTile = new MapTile(null, pos,"HeroStorm");
         assertNull(mapTile.getEffect());
     }
 
     @Test
     void createMapTileNullPosition() {
         assertThrows(IllegalArgumentException.class, () -> {
-            new MapTile(new Effect(), null);
+            new MapTile(new Effect(), null, "HeroStorm");
+        });
+    }
+    @Test
+    void createMapTileNullName(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            new MapTile(new Effect(), new Position(0,0), null);
+        });
+    }
+
+    @Test
+    void createMapTileEmptyStringName(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            new MapTile(new Effect(), new Position(0,0), "");
         });
     }
 
     @Test
     void getEffect() {
         Effect e = new Effect();
-        MapTile maptile = new MapTile(e, new Position(0, 0));
+        MapTile maptile = new MapTile(e, new Position(0, 0), "HeroStorm");
         assertEquals(maptile.getEffect(), e);
     }
 
     @Test
     void getEffectNull(){
-        MapTile maptile = new MapTile(null, new Position(0, 0));
+        MapTile maptile = new MapTile(null, new Position(0, 0), "HeroStorm");
         assertNull(maptile.getEffect());
+    }
 
-
+    @Test
+    void getName() {
+        MapTile maptile = new MapTile(new Effect(), new Position(0, 0), "HeroStorm");
+        assertEquals(maptile.getName(), "HeroStorm");
     }
 
 }

15886bfdbee7fb8150f789784463e327b5ed76c0
Hojjat Seyed <hojjatseyed@gmail.com>
2019-10-17 20:45:04 +0200

    SetPosition done in MapTile & Tests written for it

--- src/main/java/MapTile.java
+++ src/main/java/MapTile.java
@@ -9,28 +9,43 @@ public class MapTile {
     private MapTile westTile;
 
     // Effect can be null, no effect on tile
-    public MapTile(Effect effect, Position position, String name, MapTile previousTile, Directions direction) {
-        this(effect, position, name);
+    public MapTile(Effect effect, String name, MapTile previousTile, Directions direction) {
+        this(effect, name);
+        setPosition(previousTile, direction);
         // Setting of tile
+
 
     }
 
-    public MapTile(Effect effect, Position position, String name) {
-        if (position == null)
-            throw new IllegalArgumentException("Position can't be null ");
+    public MapTile(Effect effect, String name) {
         if (name == null)
             throw new IllegalArgumentException("Name can't be null");
         if (name.trim().equals(""))
             throw new IllegalArgumentException("Name can't be empty");
         this.effect = effect;
-        this.position = position;
+        this.position = new Position(0, 0);
         this.name = name;
-
-
     }
 
     private void setTile(MapTile prev, Directions dir) {
 
+    }
+
+    private void setPosition(MapTile prev, Directions dir) {
+        switch (dir) {
+            case EAST:
+                this.position = new Position(prev.getPosition().getX() + 1, prev.getPosition().getY());
+                break;
+            case NORTH:
+                this.position = new Position(prev.getPosition().getX(), prev.getPosition().getY() + 1);
+                break;
+            case WEST:
+                this.position = new Position(prev.getPosition().getX() - 1, prev.getPosition().getY());
+                break;
+            case SOUTH:
+                this.position = new Position(prev.getPosition().getX(), prev.getPosition().getY() - 1);
+                break;
+        }
     }
 
     public Effect getEffect() {
--- src/test/java/MapTileTest.java
+++ src/test/java/MapTileTest.java
@@ -4,69 +4,111 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MapTileTest {
 
+    MapTile startTile = new MapTile(new Effect(), "StartWood");
+
+
     @Test
-    void createMapTile() {
+    void createStartTile() {
         Effect e = new Effect();
-        Position pos = new Position(0, 0);
-        MapTile mapTile = new MapTile(e, pos,"HeroStorm");
+        String name = "StartTile";
+        MapTile mapTile = new MapTile(e, "StartTile");
         assertEquals(mapTile.getEffect(), e);
-        assertEquals(mapTile.getPosition(), pos);
+        assertEquals(mapTile.getPosition().getX(), 0);
+        assertEquals(mapTile.getPosition().getY(), 0);
+        assertEquals(mapTile.getName(), name);
+
     }
+
+    @Test
+    void createMapTileToEastOfStarterTile() {
+        Effect e = new Effect();
+        MapTile mapTile = new MapTile(e, "HeroStorm", startTile, Directions.EAST);
+        assertEquals(mapTile.getEffect(), e);
+        assertEquals(mapTile.getPosition().getX(), startTile.getPosition().getX() + 1);
+        assertEquals(mapTile.getPosition().getY(), startTile.getPosition().getY());
+    }
+
+    @Test
+    void createMapTileToNorthOfStarterTile() {
+        Effect e = new Effect();
+        MapTile mapTile = new MapTile(e, "HeroStorm", startTile, Directions.NORTH);
+        assertEquals(mapTile.getEffect(), e);
+        assertEquals(mapTile.getPosition().getX(), startTile.getPosition().getX());
+        assertEquals(mapTile.getPosition().getY(), startTile.getPosition().getY() + 1);
+
+    }
+
+    @Test
+    void createMapTileToWestOfStarterTile() {
+        Effect e = new Effect();
+        MapTile mapTile = new MapTile(e, "HeroStorm", startTile, Directions.WEST);
+        assertEquals(mapTile.getEffect(), e);
+        assertEquals(mapTile.getPosition().getX(), startTile.getPosition().getX() - 1);
+        assertEquals(mapTile.getPosition().getY(), startTile.getPosition().getY());
+    }
+
+    @Test
+    void createMapTileToSouthOfStarterTile(){
+        Effect e = new Effect();
+        MapTile mapTile = new MapTile(e, "HeroStorm", startTile, Directions.SOUTH);
+        assertEquals(mapTile.getEffect(), e);
+        assertEquals(mapTile.getPosition().getX(), startTile.getPosition().getX());
+        assertEquals(mapTile.getPosition().getY(), startTile.getPosition().getY() - 1);
+    }
+
 
     @Test
     void createMapTileNullEffect() {
-        Position pos = new Position(0, 0);
-        MapTile mapTile = new MapTile(null, pos,"HeroStorm");
+        MapTile mapTile = new MapTile(null, "HeroStorm");
         assertNull(mapTile.getEffect());
     }
 
+
+
     @Test
-    void createMapTileNullPosition() {
+    void createMapTileNullName() {
         assertThrows(IllegalArgumentException.class, () -> {
-            new MapTile(new Effect(), null, "HeroStorm");
-        });
-    }
-    @Test
-    void createMapTileNullName(){
-        assertThrows(IllegalArgumentException.class, () -> {
-            new MapTile(new Effect(), new Position(0,0), null);
+            new MapTile(new Effect(), null);
         });
     }
 
     @Test
-    void createMapTileEmptyStringName(){
+    void createMapTileEmptyStringName() {
         assertThrows(IllegalArgumentException.class, () -> {
-            new MapTile(new Effect(), new Position(0,0), "");
+            new MapTile(new Effect(), "");
         });
     }
 
     @Test
-    void createMapTileWhitespacesName(){
+    void createMapTileWhitespacesName() {
         assertThrows(IllegalArgumentException.class, () -> {
-            new MapTile(new Effect(), new Position(0,0),"   ");
+            new MapTile(new Effect(), "   ");
         });
     }
 
     @Test
     void getEffect() {
         Effect e = new Effect();
-        MapTile maptile = new MapTile(e, new Position(0, 0), "HeroStorm");
+        MapTile maptile = new MapTile(e, "HeroStorm");
         assertEquals(maptile.getEffect(), e);
     }
 
     @Test
-    void getEffectNull(){
-        MapTile maptile = new MapTile(null, new Position(0, 0), "HeroStorm");
+    void getEffectNull() {
+        MapTile maptile = new MapTile(null, "HeroStorm");
         assertNull(maptile.getEffect());
     }
 
     @Test
     void getName() {
-        MapTile maptile = new MapTile(new Effect(), new Position(0, 0), "HeroStorm");
+        MapTile maptile = new MapTile(new Effect(), "HeroStorm");
         assertEquals(maptile.getName(), "HeroStorm");
     }
 
+    @Test
+    void setTile() {
 
+    }
 
 
 }

f4b8c37ca072d00659ed9925f05474688830f020
gustavjunedahl <gustav.junedahl@gmail.com>
2019-10-23 11:26:22 +0200

    Added tests for position to bump coverage

--- src/test/java/PositionTest.java
+++ src/test/java/PositionTest.java
@@ -1,16 +1,58 @@
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class PositionTest {
 
+    Position p = new Position(1,1);
+
+    @Test
+    void getX(){
+        assertEquals(p.getX(), 1);
+    }
+
+    @Test
+    void getY(){
+        assertEquals(p.getY(), 1);
+    }
+
+    @Test
+    void setXPositive(){
+        p.setX(2);
+        assertEquals(p.getX(), 2);
+    }
+
+    @Test
+    void setYPositive(){
+        p.setY(2);
+        assertEquals(p.getY(), 2);
+    }
+
+    @Test
+    void setXNegative(){
+        p.setX(-2);
+        assertEquals(p.getX(), -2);
+    }
+
+    @Test
+    void setYNegative(){
+        p.setY(-2);
+        assertEquals(p.getY(), -2);
+    }
 
     @Test
     void equals(){
         Position pos1 = new Position(3,3);
         Position pos2 = new Position(3,3);
-        //assertTrue(pos1.equals(pos2));
         assertEquals(pos1,pos2);
+    }
+
+    @Test
+    void equalsNotPosition(){
+        Position pos1 = new Position(3,3);
+        Object obj = new Object();
+        assertNotEquals(pos1,obj);
     }
 
 }

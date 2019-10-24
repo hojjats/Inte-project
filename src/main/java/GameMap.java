1be8826100e3f3ec0a89acd9eb2a1a12f70a3eb2
Hojjat Seyed <hojjatseyed@gmail.com>
2019-10-24 10:46:28 +0200

    getDirection method added

--- src/main/java/GameMap.java
+++ src/main/java/GameMap.java
@@ -1,5 +1,43 @@
-public class GameMap {
+import java.util.HashMap;
+import java.util.Map;
 
+public class GameMap {
+    private Map<Position, MapTile> tiles = new HashMap<>();
+
+    public GameMap() {
+        tiles.put(new Position(0, 0), new MapTile(new Effect(), "startTile"));
+
+    }
+
+    public MapTile getTile(Position pos) {
+        return tiles.get(pos);
+    }
+
+    public MapTile move(MapTile currentTile, Position newPos) {
+        if (tiles.get(newPos) != null)
+            return tiles.get(newPos);
+
+
+        return null;
+    }
+
+    public int getDiscoveredTiles() {
+        return tiles.size();
+    }
+
+    public Directions getDirection(MapTile currentTile, Position newPosition){
+        if (currentTile.getPosition().x == newPosition.getX() && currentTile.getPosition().getY() +1 == newPosition.getY())
+            return Directions.NORTH;
+        if (currentTile.getPosition().x == newPosition.getX() && currentTile.getPosition().getY() -1 == newPosition.getY())
+            return Directions.SOUTH;
+        if (currentTile.getPosition().x -1 == newPosition.getX() && currentTile.getPosition().getY() == newPosition.getY())
+            return Directions.WEST;
+        if (currentTile.getPosition().x +1 == newPosition.getX() && currentTile.getPosition().getY() == newPosition.getY())
+            return Directions.EAST;
+        return null;
+    }
+
+/*
     private MapTile[][] mapTiles;
     private int dimensions;
 
@@ -27,5 +65,7 @@ public class GameMap {
         int tempX = pos.getX();
         int tempY = pos.getY();
         return (tempX <= dimensions && tempX > 0 && tempY <= dimensions && tempY > 0);
-    }
+    }*/
+
+
 }

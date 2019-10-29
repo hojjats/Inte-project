1567a7571b87da8e28363b359bbf88beecd5b9af
Hojjat Seyed <hojjatseyed@gmail.com>
2019-10-29 09:21:03 +0100

    Refactorated in enemy

--- src/main/java/Enemy.java
+++ src/main/java/Enemy.java
@@ -4,18 +4,14 @@ public class Enemy extends Creature {
     private String name;
     private Dice dice;
 
-    public Enemy(String name,int strength,int speed, boolean isAggressive){
+    public Enemy(String name, int strength, int speed, boolean isAggressive) {
         this(name, strength, speed, isAggressive, new Dice());
     }
 
-    public Enemy(String name,int strength,int speed, boolean isAggressive, Dice dice) {
-        super(speed,strength);
+    public Enemy(String name, int strength, int speed, boolean isAggressive, Dice dice) {
+        super(speed, strength);
         this.isAggressive = isAggressive;
-        if (name == null)
-            throw new IllegalArgumentException("Enemy must have a name");
-        if (name.length() == 0)
-            throw new IllegalArgumentException("Enemy must have a name");
-        if (name.trim().length() == 0)
+        if (name == null || name.trim().length() == 0)
             throw new IllegalArgumentException("Enemy must have a name");
         if (name.length() > 20)
             throw new IllegalArgumentException("Enemy name is too long");
@@ -23,41 +19,38 @@ public class Enemy extends Creature {
         this.dice = dice;
     }
 
-    public Enemy(Dice dice){
-        this("Grunt", 2,5, false,dice);
+    public Enemy(Dice dice) {
+        this("Grunt", 2, 5, false, dice);
     }
 
-    public Enemy(){
-        this("Grunt", 2,5, false);
+    public Enemy() {
+        this("Grunt", 2, 5, false);
     }
 
-    public int attack(){
+    public int attack() {
         return dice.roll(getStrength(), 6);
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public boolean isAggressive(){
+    public boolean isAggressive() {
         return isAggressive;
     }
 
-    public String takeDamage(int damageAmount){
-        if(getStrength()<= damageAmount){
+    public String takeDamage(int damageAmount) {
+        if (getStrength() <= damageAmount) {
             die();
-            return name +" takes "+damageAmount+" damage and Dies!";
+            return name + " takes " + damageAmount + " damage and Dies!";
         }
-        setStrength(getStrength()-damageAmount);
-        return name +" takes "+ damageAmount+" damage";
+        setStrength(getStrength() - damageAmount);
+        return name + " takes " + damageAmount + " damage";
     }
 
-   public Dice getDice(){
+    public Dice getDice() {
         return dice;
-   }
-
-
-
+    }
 
 
 }
